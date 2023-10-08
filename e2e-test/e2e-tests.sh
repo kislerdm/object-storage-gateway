01a7e92a4a0178d66143840199f68dfd47ba2084
Dmitry Kisler <admin@dkisler.com>
2023-10-09 01:43:05 +0200

    chore: moved and extended e2e tests script.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

---
+++ e2e-test/e2e-tests.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+URL="http://localhost:3000/object"
+URL_TOFU="https://github.com/opentofu/opentofu/releases/download/v1.6.0-alpha1/tofu_1.6.0-alpha1_darwin_arm64.zip"
+
+echo "Init"
+
+if [ ! -d ${PWD}/samples ]; then mkdir -p ${PWD}/samples/get; fi
+
+function deleteSamples() {
+    cd .. && rm -r ${PWD}/samples
+}
+
+cd samples || exit 1
+
+echo "generate dummy file tinytextfile.txt"
+echo "foo bar baz" >> tinytextfile.txt
+
+echo "download test sample from ${URL_TOFU} to tofu.zip"
+curl -s -Lo tofu.zip ${URL_TOFU}
+if [ $? -gt 0 ]; then echo "error" && deleteSamples; exit 1; fi
+
+echo "unzip tofu.zip"
+unzip -qq tofu.zip
+if [ $? -gt 0 ]; then echo "error" && deleteSamples; exit 1; fi
+
+echo "Run tests"
+
+files=( tinytextfile.txt LICENSE tofu.zip tofu )
+objects=( tinytextfile LICENSE tofu tofu )
+
+for i in "${!files[@]}"; do
+
+  fileName=${files[$i]}
+  objectID=${objects[$i]}
+
+  echo "upload ${fileName} as objectID ${objectID}"
+  curl -T ${fileName} ${URL}/${objectID}
+
+  echo "download ${objectID} to ./get/${fileName}"
+  curl -o ./get/${fileName} ${URL}/${objectID}
+
+  if [ "$(grep -e "error" ./get/${fileName} | wc -l)" -gt 0 ]; then
+    echo "downloading error"
+    deleteSamples
+    exit 1
+  fi
+
+  echo "compare files"
+
+  if [ "$(diff "./get/${fileName}" "${fileName}" | wc -l)" -gt 0 ]; then
+    echo "FAIL"
+    deleteSamples
+    exit 1
+  fi
+
+  echo "OK"
+
+done
+
+#deleteSamples
+
+echo "Completed"
